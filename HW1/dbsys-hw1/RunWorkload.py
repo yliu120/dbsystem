cbf73398861a272edb4803365b3befee05206ad3
Yunlong Liu <yliu120@jhmi.edu>
2015-02-21 22:01:15 -0500

    plot.py

--- HW1/dbsys-hw1/RunWorkload.py
+++ HW1/dbsys-hw1/RunWorkload.py
@@ -12,8 +12,20 @@ if sys.argv[1] == "s":
 	StorageFile.defaultPageClass = SlottedPage;
 if sys.argv[1] == "p":
 	StorageFile.defaultPageClass = PaxPage;
+	
+import os
+def get_size(start_path = './data/'):
 
-datadir = '/home/cs416/datasets/tpch-sf1'
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(start_path):
+                for f in filenames:
+                        if "rel" in f:
+                                print(f)
+                                fp = os.path.join(dirpath, f)
+                                total_size += os.path.getsize(fp)
+        return total_size/1024
+
+datadir = '/home/cs416/datasets/tpch-sf0.1'
 scaleFactor = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 pageSize = [4096, 32768]
 workloadMode = [1, 2, 3, 4]
@@ -22,4 +34,4 @@ for ps in pageSize:
 	for wlm in workloadMode:
 		for sf in scaleFactor:
 			WorkloadGenerator().runWorkload(datadir, sf, ps, wlm)
-			
+			print( get_size() )
--- HW1/dbsys-hw1/plot.py
+++ HW1/dbsys-hw1/plot.py
@@ -8,11 +8,11 @@ import numpy as np
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-
 pageTitle = sys.argv[1]
 
 sf = np.arange(0.0, 1.1, 0.1)
 data_time = []
+size = []
 # data_tuplenum = []
 for line in sys.stdin:
 	strlist = line.rstrip().split(":")
@@ -20,6 +20,8 @@ for line in sys.stdin:
 #		data_tuplenum.append(int(strlist[1]))
 	if strlist[0] == "Execution time":
 		data_time.append(float(strlist[1]))
+	if strlist[0] == "Size":
+		size.append( float(strlist[1]) );
 
 plt.figure(1)
 # plt.subplot(211)
@@ -55,3 +57,39 @@ plt.title('Size of Storage Files After Running Workload')
 plt.plot(sf, np.cos(2*np.pi*t2), 'r--')
 """
 plt.savefig('timefig-' + pageTitle + '.png');
+
+plt.figure(1);
+plt.xlabel('Scale Factor')
+plt.ylabel('Storage File Size (kB)')
+plt.title('Storage File Size - Workload: ' + pageTitle);
+
+lines = plt.plot(sf, size[0:11], 'r^-', 
+         sf, size[11:22], 'b^-',
+         sf, size[22:33], 'g^-',
+         sf, size[33:44], 'k^-',
+         sf, size[44:55], 'ro-',
+         sf, size[55:66], 'bo-',
+         sf, size[66:77], 'go-',
+         sf, size[77:], 'ko-')
+
+plt.setp(lines[0], label = '4KB Page, Mode 1')
+plt.setp(lines[1], label = '4KB Page, Mode 2')
+plt.setp(lines[2], label = '4KB Page, Mode 3')
+plt.setp(lines[3], label = '4KB Page, Mode 4')
+plt.setp(lines[4], label = '32KB Page, Mode 1')
+plt.setp(lines[5], label = '32KB Page, Mode 2')
+plt.setp(lines[6], label = '32KB Page, Mode 3')
+plt.setp(lines[7], label = '32KB Page, Mode 4')
+
+
+plt.legend(loc='upper left', fontsize = 'x-small', ncol=2, borderaxespad=0.)
+"""
+plt.subplot(212)
+plt.xlabel('Scale Factor')
+plt.ylabel('Size(kilobytes)')
+plt.title('Size of Storage Files After Running Workload')
+plt.plot(sf, np.cos(2*np.pi*t2), 'r--')
+"""
+plt.savefig('sizefig-' + pageTitle + '.png');
+
+
